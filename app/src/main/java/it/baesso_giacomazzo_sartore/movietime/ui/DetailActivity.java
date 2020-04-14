41b922c2433f28afa2336481bd161f52e05fc58f
gioGiaco <giorgia.giaco@gmail.com>
2020-04-14 17:24:40 +0200

    Detail layout

---
+++ app/src/main/java/it/baesso_giacomazzo_sartore/movietime/ui/DetailActivity.java
@@ -0,0 +1,16 @@
+package it.baesso_giacomazzo_sartore.movietime.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.baesso_giacomazzo_sartore.movietime.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
